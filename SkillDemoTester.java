aea6c79a8abde6c7a4a51119c0422c43bf39ac6b
ThisPorker <riskx70@gmail.com>
2022-05-11 22:36:25 -0700

    added failure test and libraries

---
+++ SkillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    @Test
+    public void addition() {
+        assertEquals(4, SkillDemo.add(1, 1));
+    }
+}
